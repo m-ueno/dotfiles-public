fe0b51bbe0bd7445b87e36a391c3deca0040a2bd
uenop <dummy@mail.com>
2012-10-18 16:50:29 +0900

    fix

--- install-dropbox.sh
+++ install-dropbox.sh
@@ -1,3 +1,5 @@
 echo run ~/.dropbox-dist/dropboxd
 cd ~ && wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf -
-curl -L https://www.dropbox.com/download?dl=packages/dropbox.py > ~/bin
+mkdir -p ~/bin
+wget -O ~/bin/dropbox.py "http://www.dropbox.com/download?dl=packages/dropbox.py"
+chmod u+x ~/bin/dropbox.py
