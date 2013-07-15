3bcb2f96d5bd0156e1f4767bf7334ceeaf497d36
mueno-mba <dummy@mail.com>
2013-07-15 21:56:40 +0900

    openstack completion / downloader

---
+++ update-ostack-completion.sh
@@ -0,0 +1,16 @@
+rc_file=~/.dotfiles/.zshrc.ostack
+
+_update_ostack_completion() {
+    a=(nova neutron keystone)
+    echo $a
+    echo '' > $rc_file
+    for i in $@ ; do
+        echo "install: $i"
+        echo "\n# $i" >> $rc_file
+        curl -L "https://raw.github.com/openstack/python-${i}client/master/tools/${i}.bash_completion" >> $rc_file
+    done
+}
+# https://raw.github.com/openstack/python-novaclient/master/tools/nova.bash_completion
+# https://raw.github.com/openstack/python-neutronclient/master/tools/neutron.bash_completion
+
+_update_ostack_completion nova neutron keystone
