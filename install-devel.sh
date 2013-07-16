f910d807f9a656eaede8cbe4046cbb8f0e63abce
mueno-mba <dummy@mail.com>
2013-07-17 08:57:54 +0900

    rename

--- install-devel.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sudo apt-get update
-sudo apt-get install -y curl git zsh tmux screen
-sudo apt-get install -y vim w3m
---
+++ setup-debian.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+alias apt-get='sudo apt-get'
+alias ai='apt-get install -y'
+
+ai curl git zsh tmux screen tree tree
+ai -y vim w3m curl wget ntp tig rsync
