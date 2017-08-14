29b0e5eae71518b73a1e66ebefd2ba0651864994
m-ueno <m-ueno@users.noreply.github.com>
2017-08-14 23:02:20 +0900

    zplug v2 init

--- zsh/zplug-init.zsh
+++ zsh/zplug-init.zsh
@@ -1,10 +1,4 @@
-# zplug options
-#   see http://qiita.com/b4b4r07/items/cd326cd31e01955b788b
-typeset -x ZPLUG_HOME=$DOTFILES/zsh/zplug
-typeset -x ZPLUG_EXTERNAL=$DOTFILES/zsh/zplug-plugins.zsh
-typeset -x ZPLUG_USE_CACHE=true
-
-source $ZPLUG_HOME/zplug
+source $DOTFILES/zplug/init.zsh
 
 # zplug update --self
 zplug check || zplug install
